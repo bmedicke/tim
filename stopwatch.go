ecd12bae0d198f7906d441c099faf76834070a4a
Benjamin Medicke <bmedicke@gmail.com>
2022-04-23 12:57:13 +0200

    handle events in goroutine

--- stopwatch.go
+++ stopwatch.go
@@ -13,35 +13,35 @@ const stopwatchInstructions = "press q or esc to stop"
 func stopwatch(quietFlag bool) {
 	screen := setupScreen()
 
+	go handleStopwatchEvents(screen)
+
 	for {
-		if screen.HasPendingEvent() {
-			handleStopwatchEvent(screen)
-		} else {
-			if !quietFlag {
-				screen.Show() // update screen.
-				duration := fmt.Sprintf("%s", time.Now().Sub(start))
-				drawText(screen, 0, 1, 30, 2, duration)
-				drawText(screen, 0, 0, 30, 1, stopwatchInstructions)
-			}
+		if !quietFlag {
+			screen.Show() // update screen.
+			duration := fmt.Sprintf("%s", time.Now().Sub(start))
+			drawText(screen, 0, 1, 30, 2, duration)
+			drawText(screen, 0, 0, 30, 1, stopwatchInstructions)
 		}
 		time.Sleep(time.Millisecond * 50)
 	}
 }
 
-func handleStopwatchEvent(screen tcell.Screen) {
-	ev := screen.PollEvent() // fetch event.
+func handleStopwatchEvents(screen tcell.Screen) {
+	for {
+		ev := screen.PollEvent() // wait for and fetch event.
 
-	// process event:
-	switch ev := ev.(type) {
-	case *tcell.EventKey:
-		if ev.Rune() == 'q' || ev.Key() == tcell.KeyEsc {
-			exitStopwatchWith(0, screen)
+		// process event:
+		switch ev := ev.(type) {
+		case *tcell.EventKey:
+			if ev.Rune() == 'q' || ev.Key() == tcell.KeyEsc {
+				exitStopwatchWith(0, screen)
+			}
+			if ev.Key() == tcell.KeyCtrlC {
+				exitStopwatchWith(127, screen)
+			}
+		case *tcell.EventResize:
+			screen.Sync()
 		}
-		if ev.Key() == tcell.KeyCtrlC {
-			exitStopwatchWith(127, screen)
-		}
-	case *tcell.EventResize:
-		screen.Sync()
 	}
 }
 
